fe3b54ecb327854c5216271ff8185573391277c4
ian morton <ian.happytuesdays@gmail.com>
2018-11-10 19:48:33 +0000

    From cli

--- src/store.js
+++ src/store.js
@@ -5,7 +5,13 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-
+    time: '1234',
+    players: [
+      { name: 'fred', hole: 1, score: -4, update: '2345' },
+      { name: 'jim', hole: 1, score: -3, update: '2345' },
+      { name: 'alan', hole: 4, score: -1, update: '3456' },
+      { name: 'greg', hole: 6, score: -1, update: '6543' }      
+    ]
   },
   mutations: {
 
