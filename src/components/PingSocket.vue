d5f640f1338d840adaaaa5dce417d88d728a597c
Ian Morton <IanM@Leos-MacBook-Pro-2.local>
2018-11-11 22:19:26 +0000

    sync messageChannel

--- src/components/PingSocket.vue
+++ src/components/PingSocket.vue
@@ -36,7 +36,7 @@ export default {
   methods: {
     pingServer() {
       // Send the "pingServer" event to the server.
-      this.$socket.emit('message', 'PING!')
+      this.$socket.emit('messageChannel', 'PING!')
     }
   }
 }
